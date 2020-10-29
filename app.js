5a2e2a46a89dbcfbe2a8c89b7107e4c3771a051a
Abraham E. Tavarez <abetavarez@icloud.com>
2020-10-28 22:22:16 -0400

    app setup

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import session from 'express-sessions';
+import cookieParser from 'cookie-parser';
+import flash from 'flash';
+
+import routes from './routes.js';
+
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+const PORT = 5000;
+app.set('port', process.env.PORT || PORT);
+
+// View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Middleware Stack
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  session({
+    secret: '',
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+app.use(flash());
+
+// routes
+app.use(routes);
+
+// Listening
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port: ${app.get('port')}`);
+});
